95ece529094fc30c4f9acbe07dd5d68bd8294ce3
Cristhian Filigrana <filigrana1985@gmail.com>
2023-06-16 00:55:37 -0500

    1er Entrega Proyecto Final

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h2>Iniciar sesión</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" v-model="password" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      login() {
+        // Lógica de inicio de sesión aquí
+        // Enviar datos al servidor, realizar validaciones, etc.
+        console.log('Inicio de sesión:', this.email, this.password);
+      }
+    }
+  };
+  </script>
+  
---
+++ src/components/Registro.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h2>Registro</h2>
+      <form @submit.prevent="register">
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" v-model="password" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Registrarse</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      register() {
+        // Lógica de registro aquí
+        // Enviar datos al servidor, realizar validaciones, etc.
+        console.log('Registro:', this.email, this.password);
+      }
+    }
+  };
+  </script>
+  
